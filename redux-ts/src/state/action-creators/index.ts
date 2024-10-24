6ca037cd1de884577de40e555975a5e2f51b2cf4
Hoàng Giang <gianghoang.uet.vnu@gmail.com>
2024-10-24 16:38:56 +0700

    Update

--- redux-ts/src/state/action-creators/index.ts
+++ redux-ts/src/state/action-creators/index.ts
@@ -10,7 +10,12 @@ const searchRepositories = (term: string) => {
   };
 
   try {
-    const {data} = await axios.get('https://registry.npmjs.org/-/v1/search')
+    const { data } = await axios.get("https://registry.npmjs.org/-/v1/search",
+      params: {
+        text: term
+      }
+    );
+    data.objects.map(()=> {})
   } catch (error) {
     dispatchEvent({
       type: ActionType.SEARCH_REPOSITORIES_ERROR,
